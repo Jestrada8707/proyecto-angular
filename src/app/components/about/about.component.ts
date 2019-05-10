59aeaf9c15d5db7ee8b1f446532199f264e1ffbd
Julian Estrada <julianestrada@Julian-Estradas-MacBook-Pro.local>
2019-05-10 09:00:23 -0500

    Firts project with angular, Js, Ts and nodejs

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+    public title: string;
+    public subtitle: string;
+    public web: string;
+  constructor() { 
+   this.title = 'Julian Estrada';
+   this.subtitle = 'Desarrollador Web';
+   this.web= 'julianestrada.info';
+  }
+
+  ngOnInit() {
+  }
+
+}
